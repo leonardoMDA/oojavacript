669a4637e2895df3d7131b4c40e2aa2ff781223b
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-06-20 08:36:40 -0300

    Add files via upload

---
+++ aninhados.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome:"joão",
+    idade: 24,
+    email:"joao@larissa.com",
+    telefone:["(41) 9999-0000","(41) (9988-0000)"],
+    complemento : "ap 934",
+};
+cliente.endereço = [
+{
+    rua : "R. joseph climber",
+    numero : 1337,
+    apartamento : true,
+    complemento : "Ap 934",
+},
+];
+
+console.log(cliente.endereço);
