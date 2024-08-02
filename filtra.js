3c88b8f8e86bfe1ca78402404fb323ee7211563b
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-08-02 12:12:03 -0300

    const clientes = require("./clientes.json");

---
+++ filtra.js
@@ -0,0 +1,14 @@
+const clientes = require("./clientes.json");
+
+function filtrarApartamentoSemComplementos(clientes){
+    return clientes.filter((cliente) => {
+        return(
+            cliente.endereco.apartamento &&
+            !cliente.endereco.hasOwnProperty("complemento")
+        )
+    });
+};
+
+const filtrados = filtrarApartamentoSemComplementos(clientes);
+
+console.log(filtrados)
---
+++ ordena.js
@@ -0,0 +1,19 @@
+const clientes = require("./clientes.json");
+
+function ordenar (lista, propriedades){
+    const resultado = lista.sort((a,b) =>{
+        if (a[propriedade] < b[propriedade]){
+            return -1;
+        }
+        if (a[propriedade] > b[propriedade]){
+            return 1;
+        }
+        return 0;
+    });
+
+    return resultado;
+};
+
+const ordenaNome = ordenar(clientes, "nome");
+
+console.log(ordenaNome);
